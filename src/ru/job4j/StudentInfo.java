b189895b7d561d385d1afe298c4d8d2ffb9ab076
korolvd <ffirin@mail.ru>
2020-12-06 14:35:23 +0500

    3.3. Разбор первой программы. [#422919]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vitaliy Korol");
+        System.out.println("11.12.1990");
+    }
+}
