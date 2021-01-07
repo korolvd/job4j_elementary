4b6db1074f609e7f562e25e9d10326a31ec115a4
korolvd <ffirin@mail.ru>
2021-01-07 12:54:28 +0500

    1.1.1. Конструктор по умолчанию [#423066]

---
+++ src/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+        desc = text;
+    }
+
+    public static void main(String[] args) {
+        Fix obj = new Fix("Some desc");
+    }
+}
